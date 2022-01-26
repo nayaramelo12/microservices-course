92bafa9cdc86e4a832c335f80ca03f2b091c35fa
Nayara <nayara.museologia@gmail.com>
2022-01-26 17:56:04 -0300

    implementa jwt

---
+++ hr-oauth/src/main/java/com/github/hroauth/entities/Role.java
@@ -0,0 +1,25 @@
+package com.github.hroauth.entities;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String roleName;
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+}
--- hr-user/src/main/java/com/github/hruser/entities/User.java
+++ hr-user/src/main/java/com/github/hruser/entities/User.java
@@ -39,8 +39,9 @@ public class User implements Serializable{
 	
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role",
-			joinColumns = @JoinColumn(name = "user_id"),
-			inverseJoinColumns = @JoinColumn(name = "role_id"))
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id")			
+	)
 	private Set<Role> roles = new HashSet<>();
 	
 }
