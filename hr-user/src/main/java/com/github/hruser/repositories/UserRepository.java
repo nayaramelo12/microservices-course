506f52b1c6dec342f4b0a9478910e339d2ac2dba
Nayara <nayara.museologia@gmail.com>
2022-01-28 17:59:28 -0300

    implementa as configurações de docker e cria os containers

--- hr-user/src/main/java/com/github/hruser/repositories/UserRepository.java
+++ hr-user/src/main/java/com/github/hruser/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package com.github.hruser.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.github.hruser.entities.User;
+import org.springframework.stereotype.Repository;
 
+import com.github.hruser.entities.User;
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	User findByEmail(String email);
--- hr-worker/src/main/java/com/github/hrworker/repositories/WorkerRepository.java
+++ hr-worker/src/main/java/com/github/hrworker/repositories/WorkerRepository.java
@@ -1,8 +1,11 @@
 package com.github.hrworker.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.github.hrworker.entities.Worker;
 
+@Repository
 public interface WorkerRepository extends JpaRepository<Worker, Long>{
 
 }
